81c1d305d16aef8f7368606edebc28daba6341bf
Niall Colbert <niallcolbert87@gmail.com>
2023-05-31 12:27:02 +0100

    add basic input for isbn search

---
+++ components/Booklist.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input v-model="isbn" type="text" @keyup.enter="getBook">
+    <button @click="getBook">Get book details</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookList',
+  data () {
+    return {
+      isbn: '0358380235'
+    }
+  },
+  methods: {
+    getBook() {
+      console.log("getting book", this.isbn)
+    }
+  }
+}
+</script>
